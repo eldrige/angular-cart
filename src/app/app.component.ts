a831cb9431c29efab7d6178340c8d9f4d0f08b9b
Prince Eldrige <peldrige8@gmail.com>
2020-04-09 00:43:07 +0100

    finished productlist component

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  template: 
+  `
+    <div><h1>{{title}}</h1></div>
+    <pm-products></pm-products>
+  `
+ 
+})
+export class AppComponent {
+  title = 'Angular: Hello world';
+}
