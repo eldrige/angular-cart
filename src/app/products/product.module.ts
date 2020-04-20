c84fe41e66211b941076f88af0726bac2470cbb1
Prince Eldrige <peldrige8@gmail.com>
2020-04-20 20:05:34 +0100

    redifined modules

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+
+import { ConvertToSpacesPipe } from "../shared/convert-to-spaces.pipe";
+
+import { ProductListComponent } from "../products/product-list.component";
+import { ProductDetailComponent } from "../products/product-detail.component";
+
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from '../products/product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    ProductDetailComponent,
+    
+  ],
+  imports: [
+    RouterModule.forChild([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id",canActivate: [ProductDetailGuard], component: ProductDetailComponent },
+    ]),
+    SharedModule
+  ]
+})
+export class ProductModule { }
